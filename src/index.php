7cb2a9fc019b8ccad55f54932a7d29717d45c017
Vincent Faliès <vincent.falies@wolterskluwer.com>
2017-05-12 17:16:01 +0200

    Fix test file

--- src/index.php
+++ src/index.php
@@ -1,40 +1,37 @@
 <?php
 
-require 'Tmdb.php';
-require 'Search.php';
-require 'Movie.php';
-require 'Collection.php';
-require 'SearchMovieResult.php';
+require '../vendor/autoload.php';
 
 use Vfac\Tmdb\Tmdb;
 use Vfac\Tmdb\Search;
+//use Vfac\Tmdb\SearchMovieResult;
+//use Vfac\Tmdb\SearchTVShowResult;
+//use Vfac\Tmdb\SearchCollectionResult;
 
 $tmdb = new Tmdb('62dfe9839b8937e595e325a4144702ad');
 
 $search  = new Search($tmdb);
 $results = $search->searchMovie('star wars', array('language' => 'fr-FR'));
 
-//foreach ($results as $movie)
-//{
-//    $res = $search->getMovie($movie->id);
-//
-//    echo '<pre>';
-//    var_dump($res->getTitle().' ('.$res->getReleaseDate().')');
-//    echo '</pre>';
-//}
-
-$res = $search->getMovie($results->current()->getId());
-
-echo '<pre>';
-var_dump($res->getTitle().' ('.$res->getReleaseDate().')');
-var_dump('Collection : '.$res->getCollectionId());
-
-$collection = $search->getCollection($res->getCollectionId(), array('language' => 'fr-FR'));
-
-var_dump($collection->getName());
-
-foreach ($collection->getParts() as $part)
+if ( ! is_null($results->current()))
 {
-    var_dump($part->getTitle());
+    $res = $search->getMovie($results->current()->getId());
+
+    echo '<pre>';
+    var_dump($res->getTitle().' ('.$res->getReleaseDate().')');
+    var_dump('Collection : '.$res->getCollectionId());
+
+    $collection = $search->getCollection($res->getCollectionId(), array('language' => 'fr-FR'));
+
+    var_dump($collection->getName());
+
+    foreach ($collection->getParts() as $part)
+    {
+        var_dump($part->getTitle());
+    }
+    echo '</pre>';
 }
-echo '</pre>';
+else
+{
+    echo 'Aucun résultat';
+}
