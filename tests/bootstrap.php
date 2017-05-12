c6dfcf0f293384bf1a26056fa47e9ba67aa25733
Vincent Faliès <vincent.falies@wolterskluwer.com>
2017-05-12 16:36:26 +0200

    Mise en place de la structure de tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of the Vfac TMDB package.
+ *
+ * (c) Vincent Faliès <vincent.falies@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+date_default_timezone_set('UTC');
+require __DIR__.'/../vendor/autoload.php';
