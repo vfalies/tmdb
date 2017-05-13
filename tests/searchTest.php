ff4698f74a7aafea06de83b8e8a65365370270bb
Vincent Faliès <vincent.falies@gmail.com>
2017-05-13 18:13:57 +0200

    Mise à jour du composer pour intégrer phpunit Mise à jour des chemins dans phpunit .xml Tests

--- tests/searchTest.php
+++ tests/searchTest.php
@@ -2,14 +2,19 @@
 
 namespace Vfac\Tmdb;
 
-class SearchTest extends \PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class SearchTest extends TestCase
 {
 
     /**
      * @covers Vfac\Tmdb\Search::searchMovie
+     * @test
      */
     public function testSearchMovie()
     {
+        $this->assertTrue(true);
+
         $tmdb   = new Tmdb('62dfe9839b8937e595e325a4144702ad');
         $search = new Search($tmdb);
         $responses = $search->searchMovie('star wars');
--- tests/tmdbTest.php
+++ tests/tmdbTest.php
@@ -4,5 +4,8 @@ namespace Vfac\Tmdb;
 
 class TmdbTest extends \PHPUnit_Framework_TestCase
 {
-    
+    public function testSendRequest()
+    {
+        $this->assertTrue(true);
+    }
 }
