2b255a2a79886f46a024110da79156fe31a8fba4
Vincent Faliès <vincent.falies@gmail.com>
2017-06-01 15:54:14 +0200

    Refactoring of Results class with abstract class

---
+++ src/Results/Results.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Vfac\Tmdb\Results;
+
+abstract class Results implements \Vfac\Tmdb\Interfaces\ResultsInterface
+{
+
+    protected $id            = null;
+    protected $poster_path   = null;
+    protected $backdrop_path = null;
+    protected $conf          = null;
+
+    /**
+     * Constructor
+     * @param \Vfac\Tmdb\Tmdb $tmdb
+     * @param \stdClass $result
+     * @throws \Exception
+     */
+    public function __construct(\Vfac\Tmdb\Tmdb $tmdb, \stdClass $result)
+    {
+        // Valid input object
+        $properties = get_object_vars($this);
+        foreach (array_keys($properties) as $property)
+        {
+            if ($property != 'conf' && !property_exists($result, $property))
+            {
+                throw new \Exception('Incorrect input for ' . __CLASS__ . ' object. Property "' . $property . '" not found');
+            }
+        }
+
+        // Configuration
+        $this->conf = $tmdb->getConfiguration();
+    }
+
+    /**
+     * Get poster
+     * @return string
+     */
+    public function getPoster(string $size = 'w185'): string
+    {
+        if (!isset($this->conf->images->base_url))
+        {
+            throw new \Exception('base_url configuration not found');
+        }
+        if (!in_array($size, $this->conf->images->poster_sizes))
+        {
+            throw new \Exception('Incorrect poster size : ' . $size);
+        }
+        return $this->conf->images->base_url . $size . $this->poster_path;
+    }
+
+    /**
+     * Get backdrop
+     * @return string
+     */
+    public function getBackdrop(string $size = 'w780'): string
+    {
+        if (!isset($this->conf->images->base_url))
+        {
+            throw new \Exception('base_url configuration not found');
+        }
+        if (!in_array($size, $this->conf->images->backdrop_sizes))
+        {
+            throw new \Exception('Incorrect backdrop size : ' . $size);
+        }
+        return $this->conf->images->base_url . $size . $this->backdrop_path;
+    }
+
+}
--- src/Results/TVShow.php
+++ src/Results/TVShow.php
@@ -2,17 +2,13 @@
 
 namespace Vfac\Tmdb\Results;
 
-class TVShow implements \Vfac\Tmdb\Interfaces\ResultsInterface
+class TVShow extends Results
 {
 
-    private $id             = null;
-    private $overview       = null;
-    private $first_air_date = null;
-    private $original_name  = null;
-    private $name           = null;
-    private $poster_path    = null;
-    private $backdrop_path  = null;
-    private $conf           = null;
+    protected $overview       = null;
+    protected $first_air_date = null;
+    protected $original_name  = null;
+    protected $name           = null;
 
     /**
      * Constructor
@@ -22,15 +18,7 @@ class TVShow implements \Vfac\Tmdb\Interfaces\ResultsInterface
      */
     public function __construct(\Vfac\Tmdb\Tmdb $tmdb, \stdClass $result)
     {
-        // Valid input object
-        $properties = get_object_vars($this);
-        foreach (array_keys($properties) as $property)
-        {
-            if ($property != 'conf' && ! property_exists($result, $property))
-            {
-                throw new \Exception('Incorrect input for '.__CLASS__.' object. Property "'.$property.'" not found');
-            }
-        }
+        parent::__construct($tmdb, $result);
 
         // Populate data
         $this->id             = $result->id;
@@ -40,9 +28,6 @@ class TVShow implements \Vfac\Tmdb\Interfaces\ResultsInterface
         $this->name           = $result->name;
         $this->poster_path    = $result->poster_path;
         $this->backdrop_path  = $result->backdrop_path;
-
-        // Configuration
-        $this->conf = $tmdb->getConfiguration();
     }
 
     /**
@@ -88,40 +73,6 @@ class TVShow implements \Vfac\Tmdb\Interfaces\ResultsInterface
     public function getTitle(): string
     {
         return $this->name;
-    }
-
-    /**
-     * Get tvshow poster
-     * @return string
-     */
-    public function getPoster(string $size = 'w185'): string
-    {
-        if ( ! isset($this->conf->images->base_url))
-        {
-            throw new \Exception('base_url configuration not found');
-        }
-        if ( ! in_array($size, $this->conf->images->poster_sizes))
-        {
-            throw new \Exception('Incorrect poster size : '.$size);
-        }
-        return $this->conf->images->base_url.$size.$this->poster_path;
-    }
-
-    /**
-     * Get tvshow backdrop
-     * @return string
-     */
-    public function getBackdrop(string $size = 'w780'): string
-    {
-        if ( ! isset($this->conf->images->base_url))
-        {
-            throw new \Exception('base_url configuration not found');
-        }
-        if ( ! in_array($size, $this->conf->images->backdrop_sizes))
-        {
-            throw new \Exception('Incorrect backdrop size : '.$size);
-        }
-        return $this->conf->images->base_url.$size.$this->backdrop_path;
     }
 
 }
