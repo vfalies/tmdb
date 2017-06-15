15c2325317bcba8fabfc319facd2ee416ef78d3f
Vincent Faliès <vincent.falies@gmail.com>
2017-06-15 16:49:01 +0200

    Remove useless code

--- src/Interfaces/Items/TVShowInterface.php
+++ src/Interfaces/Items/TVShowInterface.php
@@ -47,20 +47,6 @@ interface TVShowInterface
      */
     public function getNote(): float;
 
-//    /**
-//     * Get TVShow poster
-//     * @param string $size
-//     * @return string
-//     */
-//    public function getPoster(string $size = 'w185'): string;
-//
-//    /**
-//     * Get TVShow backdrop
-//     * @param string $size
-//     * @return string
-//     */
-//    public function getBackdrop(string $size = 'w780'): string;
-
     /**
      * Get TVShow number of episodes
      * @return int
