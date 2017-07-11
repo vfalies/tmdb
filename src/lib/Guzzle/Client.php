6b46a3db8784e55c4d603622117d914b60b6a9ca
Vincent Faliès <vincent.falies@wolterskluwer.com>
2017-07-11 16:09:35 +0200

    Add summary file documentation

--- src/lib/Guzzle/Client.php
+++ src/lib/Guzzle/Client.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * This file is part of the Tmdb package.
+ *
+ * (c) Vincent Faliès <vincent.falies@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+* @package Tmdb 
+* @author Vincent Faliès <vincent.falies@gmail.com>
+ * @copyright Copyright (c) 2017
+ */
+
 
 namespace vfalies\tmdb\lib\Guzzle;
 
