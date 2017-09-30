16a8fd8d9d56c117a3d7853b408a4e2bad514ac5
Vincent Faliès <vincent.falies@gmail.com>
2017-09-30 20:43:08 +0200

    Add all methods for authentification and their tests

--- src/Account.php
+++ src/Account.php
@@ -14,7 +14,7 @@
 
 namespace vfalies\tmdb;
 
-use vfalies\tmdb\Exceptions\TmdbException;
+use vfalies\tmdb\Exceptions\IncorrectParamException;
 use vfalies\tmdb\Interfaces\TmdbInterface;
 use vfalies\tmdb\lib\Guzzle\Client as HttpClient;
 use vfalies\tmdb\Exceptions\InvalidResponseException;
@@ -49,50 +49,53 @@ class Account
   }
 
   /**
-   * Account connexion
-   * @param string $redirect_to URL redirect after authentification
-   * @return void
+   * Connect and valid request token
+   * @param  string $request_token Request token
+   * @param  string|null $redirect_url Redirection url after connection (optional)
+   * @return bool
    */
-  public function connect(string $redirect_to) : void
+  public function connect(string $request_token, ?string $redirect_url = null) : bool
   {
-      try {
-          $token = $this->getTemporaryRequestToken();
-
-          header("Location: https://www.themoviedb.org/authenticate/$token?redirect_to=$redirect_to", 301);
-          exit();
-      } catch (TmdbException $e) {
-        throw $e;
+      $url = "https://www.themoviedb.org/authenticate/$request_token";
+      if (!is_null($redirect_url))
+      {
+          if (!filter_var($redirect_url, FILTER_VALIDATE_URL))
+          {
+              throw new IncorrectParamException('Invalid redirect Url');
+          }
+          $url .= "?redirect_to=$redirect_url";
       }
+      header("Location: $url");
+      return true;
   }
 
   /**
-   * Get temporary request token
-   * @return string temporary request token
-   * @throws InvalidResponseException
+   * Get a new request token
+   * @return string Request token
    */
-  private function getTemporaryRequestToken()
+  public function getRequestToken() : string
   {
       $data = $this->tmdb->sendRequest(new HttpClient(new \GuzzleHttp\Client()), '/authentification/token/new', null, []);
 
-      if (!isset($data->success) || $data->success != 'true')
+      if (!isset($data->success) || $data->success != 'true' || !isset($data->request_token))
       {
-          throw new InvalidResponseException();
+          throw new InvalidResponseException("Getting request token failed");
       }
      return $data->request_token;
   }
 
   /**
-   * Create an user session id
-   * @param  string $request_token TMDB request token, return after Account::connect() call
-   * @return string                Session ID
+   * Create a new session Account
+   * @param  string $request_token Request token create by Account::getRequestToken() and validate by Account::connect()
+   * @return string                Session token string
    */
   public function createSession(string $request_token) : string
   {
-      $data = $this->tmdb->sendRequest(new HttpClient(new \GuzzleHttp\Client()), '/authentification/token/new', null, ['request_token' => $request_token]);
+      $data = $this->tmdb->sendRequest(new HttpClient(new \GuzzleHttp\Client()), '/authentification/session/new', null, ['request_token' => $request_token]);
 
-      if (!isset($data->success) || $data->success != 'true')
+      if (!isset($data->success) || $data->success != 'true' || !isset($data->session_id))
       {
-          throw new InvalidResponseException();
+          throw new InvalidResponseException("Creating session failed");
       }
       return $data->session_id;
   }
--- tests/AccountTest.php
+++ tests/AccountTest.php
@@ -31,9 +31,89 @@ class AccountTest extends TestCase
 
     /**
      * @test
+     * @runInSeparateProcess
      */
     public function testConnect()
     {
-        $get = new Account($this->tmdb);
+        $request_token = '991c25974a2fcf3d923ae722f46e9c44788ff3ba';
+        $redirect_url  = 'https://vfac.fr';
+
+        $Account = new Account($this->tmdb);
+        $Account->connect($request_token, $redirect_url);
+
+        $this->assertContains(
+            'Location: https://www.themoviedb.org/authenticate/'.$request_token.'?redirect_to='.$redirect_url, xdebug_get_headers()
+        );
+    }
+
+    /**
+     * @test
+     * @expectedException \Exception
+     */
+    public function testConnectInvalidRediction()
+    {
+        $request_token = '991c25974a2fcf3d923ae722f46e9c44788ff3ba';
+        $redirect_url  = 'vfac.fr';
+
+        $Account = new Account($this->tmdb);
+        $Account->connect($request_token, $redirect_url);
+    }
+
+    /**
+     * @test
+     */
+    public function testGetRequestToken()
+    {
+        $json_object = json_decode(file_get_contents('tests/json/requestTokenOk.json'));
+        $this->tmdb->method('sendRequest')->willReturn($json_object);
+
+        $Account = new Account($this->tmdb);
+        $request_token = $Account->getRequestToken();
+
+        $this->assertNotEmpty($request_token);
+    }
+
+    /**
+     * @test
+     * @expectedException \Exception
+     */
+    public function testGetRequestTokenNok()
+    {
+        $json_object = json_decode(file_get_contents('tests/json/requestTokenNok.json'));
+        $this->tmdb->method('sendRequest')->willReturn($json_object);
+
+        $Account = new Account($this->tmdb);
+        $Account->getRequestToken();
+    }
+
+    /**
+     * @test
+     */
+    public function testCreateSession()
+    {
+        $request_token = '991c25974a2fcf3d923ae722f46e9c44788ff3ba';
+
+        $json_object = json_decode(file_get_contents('tests/json/sessionOk.json'));
+        $this->tmdb->method('sendRequest')->willReturn($json_object);
+
+        $Account = new Account($this->tmdb);
+        $session = $Account->createSession($request_token);
+
+        $this->assertNotEmpty($session);
+    }
+
+    /**
+     * @test
+     * @expectedException \Exception
+     */
+    public function testCreateSessionNok()
+    {
+        $request_token = '991c25974a2fcf3d923ae722f46e9c44788ff3ba';
+
+        $json_object = json_decode(file_get_contents('tests/json/sessionNok.json'));
+        $this->tmdb->method('sendRequest')->willReturn($json_object);
+
+        $Account = new Account($this->tmdb);
+        $Account->createSession($request_token);
     }
 }
